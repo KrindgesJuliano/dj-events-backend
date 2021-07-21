274c689cd025f47229b6e843f71e73198d37dcb1
Juliano Krindges <juliano9krindges@hotmail.com>
2021-07-20 22:47:58 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8c965966a787e98ddfc478d89e977c27'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3da113c2-f923-447e-a64e-1e687880255f'
+};
